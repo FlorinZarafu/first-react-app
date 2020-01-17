fd3b786b8e11a12c31ea49e693d4b8b87a1fa338
Florin Zarafu <zrflorin@gmail.com>
2020-01-16 23:31:11 -0800

    Updates

---
+++ precache-manifest.ab911626ac60f5d8e5b66ce9ce1c058b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc4b20340bd8240eb06c1aad834dcb3",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "f700ee5eef9c9ec58af6",
+    "url": "/first-react-app/static/css/main.a141d3fb.chunk.css"
+  },
+  {
+    "revision": "df8b7364f2fd1b3ea404",
+    "url": "/first-react-app/static/js/2.9ba14376.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-react-app/static/js/2.9ba14376.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f700ee5eef9c9ec58af6",
+    "url": "/first-react-app/static/js/main.7a8ae692.chunk.js"
+  },
+  {
+    "revision": "8f8421dc9130ba9a9ff5",
+    "url": "/first-react-app/static/js/runtime-main.cb0de62a.js"
+  }
+]);
